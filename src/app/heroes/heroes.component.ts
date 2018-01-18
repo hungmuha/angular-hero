86ed842a4a78daea868e8dcd01fa24a1274c6c68
hungmuha <hung.soboss@gmail.com>
2018-01-18 13:31:09 -0700

    master/detail component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,14 +8,18 @@ import {HEROES} from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+	heroes = HEROES;
+
 	selectedHero: Hero;
 	
-	onSelect(hero:Hero):void{
-		this.selectedHero = hero;
-	}
+	
   constructor() { }
 
   ngOnInit() {
   }
+
+	onSelect(hero:Hero):void{
+		this.selectedHero = hero;
+	}
 
 }
